6d5b5e2bd3c9f480ba39f6586ad6fc74ff897e32
Syarif Hidayat <syarifhidayat082001@gmail.com>
2021-06-27 20:39:04 +0700

    database

--- database/webframework.sql
+++ database/webframework.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 18 Jun 2021 pada 18.49
+-- Generation Time: 27 Jun 2021 pada 15.38
 -- Versi Server: 10.1.29-MariaDB
 -- PHP Version: 7.2.0
 
@@ -25,6 +25,27 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `fakultas`
+--
+
+CREATE TABLE `fakultas` (
+  `id_fakultas` int(11) NOT NULL,
+  `nama_fakultas` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `fakultas`
+--
+
+INSERT INTO `fakultas` (`id_fakultas`, `nama_fakultas`) VALUES
+(1, 'Elektronika'),
+(2, 'Teknologi Informasi'),
+(3, 'Teknik Mesin'),
+(4, 'Teknik Sipil');
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `mahasiswa`
 --
 
@@ -33,6 +54,7 @@ CREATE TABLE `mahasiswa` (
   `nim` varchar(15) NOT NULL,
   `nama` varchar(50) NOT NULL,
   `jekel` char(2) NOT NULL,
+  `id_fakultas` int(11) NOT NULL,
   `id_prodi` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   `alamat` varchar(100) NOT NULL,
@@ -43,8 +65,10 @@ CREATE TABLE `mahasiswa` (
 -- Dumping data untuk tabel `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `jekel`, `id_prodi`, `email`, `alamat`, `tgl_lahir`) VALUES
-(1, '100', 'Restu', 'P', 1, 'Restubaikbaik@gmail.com', 'Padang', '2021-07-02');
+INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `jekel`, `id_fakultas`, `id_prodi`, `email`, `alamat`, `tgl_lahir`) VALUES
+(1, '100', 'Restu', 'P', 2, 1, 'Restubaikbaik@gmail.com', 'Padang', '2021-07-02'),
+(2, '101', 'Cek', 'L', 2, 1, 'dns@gmail.com', 'Padang', '2021-06-16'),
+(3, '102', 'Tes', 'L', 3, 2, 'Tes@gmail.com', 'Silayang', '2021-06-25');
 
 -- --------------------------------------------------------
 
@@ -54,6 +78,7 @@ INSERT INTO `mahasiswa` (`id`, `nim`, `nama`, `jekel`, `id_prodi`, `email`, `ala
 
 CREATE TABLE `prodi` (
   `id_prodi` int(11) NOT NULL,
+  `id_fakultas` int(11) NOT NULL,
   `prodi` varchar(50) NOT NULL,
   `keterangan` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -62,41 +87,56 @@ CREATE TABLE `prodi` (
 -- Dumping data untuk tabel `prodi`
 --
 
-INSERT INTO `prodi` (`id_prodi`, `prodi`, `keterangan`) VALUES
-(1, 'TRPL', 'D4');
+INSERT INTO `prodi` (`id_prodi`, `id_fakultas`, `prodi`, `keterangan`) VALUES
+(1, 2, 'TRPL', 'Teknologi Informasi'),
+(2, 3, 'Mesin', 'Teknik Mesin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  ADD PRIMARY KEY (`id_fakultas`);
+
+--
 -- Indexes for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `id_prodi` (`id_prodi`);
+  ADD KEY `id_prodi` (`id_prodi`),
+  ADD KEY `id_fakultas` (`id_fakultas`);
 
 --
 -- Indexes for table `prodi`
 --
 ALTER TABLE `prodi`
-  ADD PRIMARY KEY (`id_prodi`);
+  ADD PRIMARY KEY (`id_prodi`),
+  ADD KEY `id_fakultas` (`id_fakultas`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `fakultas`
+--
+ALTER TABLE `fakultas`
+  MODIFY `id_fakultas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `prodi`
 --
 ALTER TABLE `prodi`
-  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_prodi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -106,7 +146,14 @@ ALTER TABLE `prodi`
 -- Ketidakleluasaan untuk tabel `mahasiswa`
 --
 ALTER TABLE `mahasiswa`
-  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE NO ACTION ON UPDATE CASCADE;
+  ADD CONSTRAINT `mahasiswa_ibfk_1` FOREIGN KEY (`id_prodi`) REFERENCES `prodi` (`id_prodi`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `mahasiswa_ibfk_2` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`);
+
+--
+-- Ketidakleluasaan untuk tabel `prodi`
+--
+ALTER TABLE `prodi`
+  ADD CONSTRAINT `prodi_ibfk_1` FOREIGN KEY (`id_fakultas`) REFERENCES `fakultas` (`id_fakultas`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
